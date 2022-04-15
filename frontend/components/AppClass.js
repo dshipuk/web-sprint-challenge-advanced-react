8779e9a304d5c125272989da16bd2b39f48f16c4
unknown <dshipuk@yahoo.com>
2022-04-14 22:31:52 -0700

    made code more readable

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import axios from 'axios'
+import React from "react";
+import axios from "axios";
 
 export default class AppClass extends React.Component {
   state = {
@@ -11,101 +11,104 @@ export default class AppClass extends React.Component {
     grid: [
       [null, null, null],
       [null, "B", null],
-      [null, null, null]
+      [null, null, null],
     ],
-  }
+  };
 
   isActive = (value) => {
     if (value === "B") {
-      return "square active"
+      return "square active";
     } else {
-      return "square"
+      return "square";
     }
-  }
+  };
 
   goUp = () => {
     if (this.state.yAxis - 1 < 0) {
       return this.setState({
-      ...this.state,
-      errorMessage: "You can't go up"
-    })
-  }
+        ...this.state,
+        errorMessage: "You can't go up",
+      });
+    }
     const initArray = [
       [null, null, null],
       [null, null, null],
-      [null, null, null]
-    ]
-    const updatedArray = initArray
-    updatedArray[this.state.yAxis - 1][this.state.xAxis] = "B"
+      [null, null, null],
+    ];
+    const updatedArray = initArray;
+    updatedArray[this.state.yAxis - 1][this.state.xAxis] = "B";
     this.setState({
       ...this.state,
       yAxis: this.state.yAxis - 1,
       total: this.state.total + 1,
       errorMessage: "",
-      grid: updatedArray
-    })
-  }
+      grid: updatedArray,
+    });
+  };
 
   goDown = () => {
-  if (this.state.yAxis + 1 > 2) return this.setState({
-      ...this.state,
-      errorMessage: "You can't go down"
-    })
+    if (this.state.yAxis + 1 > 2)
+      return this.setState({
+        ...this.state,
+        errorMessage: "You can't go down",
+      });
     const initArray = [
       [null, null, null],
       [null, null, null],
-      [null, null, null]
-    ]
-    const updatedArray = initArray
-    updatedArray[this.state.yAxis + 1][this.state.xAxis] = "B"
+      [null, null, null],
+    ];
+    const updatedArray = initArray;
+    updatedArray[this.state.yAxis + 1][this.state.xAxis] = "B";
     this.setState({
       ...this.state,
       yAxis: this.state.yAxis + 1,
       total: this.state.total + 1,
       errorMessage: "",
-      grid: updatedArray
-    })
-  }
+      grid: updatedArray,
+    });
+  };
 
   goLeft = () => {
-    if (this.state.xAxis - 1 < 0) return this.setState({
-      errorMessage: "You can't go left"
-    })
+    if (this.state.xAxis - 1 < 0)
+      return this.setState({
+        errorMessage: "You can't go left",
+      });
     const initArray = [
       [null, null, null],
       [null, null, null],
-      [null, null, null]
-    ]
-    const updatedArray = initArray
-    updatedArray[this.state.yAxis][this.state.xAxis - 1] = "B"
+      [null, null, null],
+    ];
+    const updatedArray = initArray;
+    updatedArray[this.state.yAxis][this.state.xAxis - 1] = "B";
     this.setState({
       ...this.state,
       xAxis: this.state.xAxis - 1,
       total: this.state.total + 1,
       errorMessage: "",
-      grid: updatedArray
-    })
-  }
+      grid: updatedArray,
+    });
+  };
 
   goRight = () => {
-    if (this.state.xAxis + 1 > 2) return (this.setState({
-      errorMessage: "You can't go right"
-    }))
+    if (this.state.xAxis + 1 > 2)
+      return this.setState({
+        errorMessage: "You can't go right",
+      });
     const initArray = [
       [null, null, null],
       [null, null, null],
-      [null, null, null]
-    ]
-    const updatedArray = initArray
-    updatedArray[this.state.yAxis][this.state.xAxis + 1] = "B"
+      [null, null, null],
+    ];
+    const updatedArray = initArray;
+    updatedArray[this.state.yAxis][this.state.xAxis + 1] = "B";
     this.setState({
       ...this.state,
       xAxis: this.state.xAxis + 1,
       total: this.state.total + 1,
       errorMessage: "",
-      grid: updatedArray
-    })
-  }
+      grid: updatedArray,
+    });
+  };
 
   reset = () => {
     this.setState({
@@ -118,83 +121,99 @@ export default class AppClass extends React.Component {
       grid: [
         [null, null, null],
         [null, "B", null],
-        [null, null, null]
-      ]
-    })
-  }
-
+        [null, null, null],
+      ],
+    });
+  };
 
   handleSubmit = (event) => {
-    const data = { 
-      "x": this.state.xAxis + 1,
-      "y": this.state.yAxis + 1,
-      "steps": this.state.total, 
-      "email": this.state.email 
-    }
+    const data = {
+      x: this.state.xAxis + 1,
+      y: this.state.yAxis + 1,
+      steps: this.state.total,
+      email: this.state.email,
+    };
     event.preventDefault();
-    axios.post("http://localhost:9000/api/result/", data)
-      .then(res => {
-        console.log(res.data)
+    axios
+      .post("http://localhost:9000/api/result/", data)
+      .then((res) => {
+        console.log(res.data);
         this.setState({
-          errorMessage: res.data.message
-        })
+          errorMessage: res.data.message,
+        });
       })
-      .catch(err => {
+      .catch((err) => {
         return this.setState({
           ...this.state,
-          errorMessage: err.response.data.message
-        })
+          errorMessage: err.response.data.message,
+        });
       })
       .finally(
         this.setState({
-          email: ""
+          email: "",
         })
-      )
-  }
+      );
+  };
 
   onChange = (event) => {
     this.setState({
-      email: event.target.value
-    })
-  }
+      email: event.target.value,
+    });
+  };
 
   render() {
-    const { className } = this.props
+    const { className } = this.props;
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates ({this.state.xAxis + 1}, {this.state.yAxis + 1})</h3>
-          <h3 id="steps">You moved { this.state.total } times</h3>
+          <h3 id="coordinates">
+            Coordinates ({this.state.xAxis + 1}, {this.state.yAxis + 1})
+          </h3>
+          <h3 id="steps">You moved {this.state.total} times</h3>
         </div>
         <div id="grid">
-          {
-            this.state.grid.map( (square, index) => {
-              //console.log(square, index)
-              return(
-                square.map( (subSquare, subIndex) => {
-                return (
-                  <div key={subIndex} className={this.isActive(subSquare)}>{subSquare}</div>
-                )
-              })
-              )
-            })
-          }
+          {this.state.grid.map((square, index) => {
+            //console.log(square, index)
+            return square.map((subSquare, subIndex) => {
+              return (
+                <div key={subIndex} className={this.isActive(subSquare)}>
+                  {subSquare}
+                </div>
+              );
+            });
+          })}
         </div>
         <div className="info">
           <h3 id="message">{this.state.errorMessage}</h3>
         </div>
         <div id="keypad">
-          <button id="left" onClick={() => this.goLeft()}>LEFT</button>
-          <button id="up" onClick={() => this.goUp()}>UP</button>
-          <button id="right" onClick={() => this.goRight()}>RIGHT</button>
-          <button id="down" onClick={() => this.goDown()}>DOWN</button>
-          <button id="reset" onClick={() => this.reset()}>reset</button>
+          <button id="left" onClick={() => this.goLeft()}>
+            LEFT
+          </button>
+          <button id="up" onClick={() => this.goUp()}>
+            UP
+          </button>
+          <button id="right" onClick={() => this.goRight()}>
+            RIGHT
+          </button>
+          <button id="down" onClick={() => this.goDown()}>
+            DOWN
+          </button>
+          <button id="reset" onClick={() => this.reset()}>
+            reset
+          </button>
         </div>
         <form onSubmit={this.handleSubmit}>
-          <input id="email" type="email" placeholder="type email" onChange={this.onChange} value={this.state.email}></input>
+          <input
+            id="email"
+            type="email"
+            placeholder="type email"
+            onChange={this.onChange}
+            value={this.state.email}
+          ></input>
           <input id="submit" type="submit"></input>
         </form>
       </div>
-    )
+    );
   }
 }
--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import axios from 'axios'
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function AppFunctional(props) {
   const [state, setState] = useState({
@@ -11,104 +11,106 @@ export default function AppFunctional(props) {
     grid: [
       [null, null, null],
       [null, "B", null],
-      [null, null, null]
-    ]
-  })
-  
+      [null, null, null],
+    ],
+  });
+
   const isActive = (value) => {
     if (value === "B") {
-      return "square active"
+      return "square active";
     } else {
-      return "square"
+      return "square";
     }
-  }
+  };
 
   const goUp = () => {
     if (state.yAxis - 1 < 0) {
       return setState({
-      ...state,
-      errorMessage: "You can't go up"
-    })
-  }
+        ...state,
+        errorMessage: "You can't go up",
+      });
+    }
     const initArray = [
       [null, null, null],
       [null, null, null],
-      [null, null, null]
-    ]
-    const updatedArray = initArray
-    updatedArray[state.yAxis - 1][state.xAxis] = "B"
+      [null, null, null],
+    ];
+    const updatedArray = initArray;
+    updatedArray[state.yAxis - 1][state.xAxis] = "B";
     setState({
       ...state,
       yAxis: state.yAxis - 1,
       total: state.total + 1,
       errorMessage: "",
-      grid: updatedArray
-    })
-  }
-
+      grid: updatedArray,
+    });
+  };
 
   const goDown = () => {
-    if (state.yAxis + 1 > 2) return setState({
+    if (state.yAxis + 1 > 2)
+      return setState({
         ...state,
-        errorMessage: "You can't go down"
-      })
-      const initArray = [
-        [null, null, null],
-        [null, null, null],
-        [null, null, null]
-      ]
-      const updatedArray = initArray
-      updatedArray[state.yAxis + 1][state.xAxis] = "B"
-      setState({
-        ...state,
-        yAxis: state.yAxis + 1,
-        total: state.total + 1,
-        errorMessage: "",
-        grid: updatedArray
-      })
-    }
+        errorMessage: "You can't go down",
+      });
+    const initArray = [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+    ];
+    const updatedArray = initArray;
+    updatedArray[state.yAxis + 1][state.xAxis] = "B";
+    setState({
+      ...state,
+      yAxis: state.yAxis + 1,
+      total: state.total + 1,
+      errorMessage: "",
+      grid: updatedArray,
+    });
+  };
 
   const goLeft = () => {
-    if (state.xAxis - 1 < 0) return setState({
+    if (state.xAxis - 1 < 0)
+      return setState({
         ...state,
-        errorMessage: "You can't go left"
-      })
-      const initArray = [
-        [null, null, null],
-        [null, null, null],
-        [null, null, null]
-      ]
-      const updatedArray = initArray
-      updatedArray[state.yAxis][state.xAxis - 1] = "B"
-      setState({
-        ...state,
-        xAxis: state.xAxis - 1,
-        total: state.total + 1,
-        errorMessage: "",
-        grid: updatedArray
-      })
-    }
+        errorMessage: "You can't go left",
+      });
+    const initArray = [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+    ];
+    const updatedArray = initArray;
+    updatedArray[state.yAxis][state.xAxis - 1] = "B";
+    setState({
+      ...state,
+      xAxis: state.xAxis - 1,
+      total: state.total + 1,
+      errorMessage: "",
+      grid: updatedArray,
+    });
+  };
 
   const goRight = () => {
-    if (state.xAxis + 1 > 2) return setState({
+    if (state.xAxis + 1 > 2)
+      return setState({
         ...state,
-        errorMessage: "You can't go right"
-      })
-      const initArray = [
-        [null, null, null],
-        [null, null, null],
-        [null, null, null]
-      ]
-      const updatedArray = initArray
-      updatedArray[state.yAxis][state.xAxis + 1] = "B"
-      setState({
-        ...state,
-        xAxis: state.xAxis + 1,
-        total: state.total + 1,
-        errorMessage: "",
-        grid: updatedArray
-      })
-    }
+        errorMessage: "You can't go right",
+      });
+    const initArray = [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+    ];
+    const updatedArray = initArray;
+    updatedArray[state.yAxis][state.xAxis + 1] = "B";
+    setState({
+      ...state,
+      xAxis: state.xAxis + 1,
+      total: state.total + 1,
+      errorMessage: "",
+      grid: updatedArray,
+    });
+  };
 
   const reset = () => {
     setState({
@@ -121,79 +123,95 @@ export default function AppFunctional(props) {
       grid: [
         [null, null, null],
         [null, "B", null],
-        [null, null, null]
-      ]
-    })
-  }
+        [null, null, null],
+      ],
+    });
+  };
 
-  const handleSubmit = event => {
-    const data = { 
-      "x": state.xAxis + 1,
-      "y": state.yAxis + 1,
-      "steps": state.total, 
-      "email": state.email 
-    }
-    axios.post("http://localhost:9000/api/result/", data)
-      .then(res => {
-        console.log(res.data)
+  const handleSubmit = (event) => {
+    const data = {
+      x: state.xAxis + 1,
+      y: state.yAxis + 1,
+      steps: state.total,
+      email: state.email,
+    };
+    axios
+      .post("http://localhost:9000/api/result/", data)
+      .then((res) => {
+        console.log(res.data);
         setState({
           ...state,
           errorMessage: res.data.message,
-          email: ""
-        })
+          email: "",
+        });
       })
-      .catch(err => {
+      .catch((err) => {
         return setState({
           ...state,
           errorMessage: err.response.data.message,
-          email: ""
-        })
-      })
-      event.preventDefault();
-  }
+          email: "",
+        });
+      });
+    event.preventDefault();
+  };
 
-  const onChange = event => {
+  const onChange = (event) => {
     setState({
       ...state,
-      email: event.target.value
-    })
-  }
-
+      email: event.target.value,
+    });
+  };
 
   return (
     <div id="wrapper" className={props.className}>
-        <div className="info">
-          <h3 id="coordinates">Coordinates ({state.xAxis + 1}, {state.yAxis + 1})</h3>
-          <h3 id="steps">You moved { state.total } time</h3>
+      <div className="info">
+        <h3 id="coordinates">
+          Coordinates ({state.xAxis + 1}, {state.yAxis + 1})
+        </h3>
+        <h3 id="steps">You moved {state.total} times</h3>
       </div>
       <div id="grid">
-          {
-            state.grid.map( (square, index) => {
-              //console.log(square, index)
-              return(
-                square.map( (subSquare, subIndex) => {
-                return (
-                  <div key={subIndex} className={isActive(subSquare)}>{subSquare}</div>
-                )
-              })
-              )
-            })
-          }
-        </div>
+        {state.grid.map((square, index) => {
+          //console.log(square, index)
+          return square.map((subSquare, subIndex) => {
+            return (
+              <div key={subIndex} className={isActive(subSquare)}>
+                {subSquare}
+              </div>
+            );
+          });
+        })}
+      </div>
       <div className="info">
-          <h3 id="message">{state.errorMessage}</h3>
+        <h3 id="message">{state.errorMessage}</h3>
       </div>
       <div id="keypad">
-          <button id="left" onClick={() => goLeft()}>LEFT</button>
-          <button id="up" onClick={goUp}>UP</button>
-          <button id="right" onClick={() => goRight()}>RIGHT</button>
-          <button id="down" onClick={() => goDown()}>DOWN</button>
-          <button id="reset" onClick={() => reset()}>reset</button>
-        </div>
+        <button id="left" onClick={() => goLeft()}>
+          LEFT
+        </button>
+        <button id="up" onClick={() => goUp()}>
+          UP
+        </button>
+        <button id="right" onClick={() => goRight()}>
+          RIGHT
+        </button>
+        <button id="down" onClick={() => goDown()}>
+          DOWN
+        </button>
+        <button id="reset" onClick={() => reset()}>
+          reset
+        </button>
+      </div>
       <form onSubmit={handleSubmit}>
-          <input id="email" type="email" placeholder="type email" onChange={onChange} value={state.email}></input>
-          <input id="submit" type="submit"></input>
-        </form>
+        <input
+          id="email"
+          type="email"
+          placeholder="type email"
+          onChange={onChange}
+          value={state.email}
+        ></input>
+        <input id="submit" type="submit"></input>
+      </form>
     </div>
-  )
+  );
 }
